a9b96c45324da56dcb78e85266e8e0cdcda79069
ShakirovSamat <samatx-r@mail.ru>
2022-10-12 23:02:42 +0300

    add "add" function

--- main.c
+++ main.c
@@ -13,6 +13,56 @@ typedef struct
     Node *head;
 }List;
 
+int add(List *list, int value,int index)
+{
+    if (list == NULL)
+    {
+        return -1;
+    }
+    if(list->head->value == NULL)
+    {
+        list->head->value = value;
+        return 0;
+    }
+    Node *curNode = list->head;
+    Node *prevNode = NULL;
+    Node *newNode = calloc(1, sizeof(Node));
+    newNode->value = value;
+    
+    int i = 0;
+    while(i < index)
+    {
+        if(curNode->next == NULL)
+        {
+            break;
+        }
+        prevNode = curNode;
+        curNode = curNode->next;
+        ++i;
+    }
+    
+    if (curNode->next == NULL && i < index - 1)
+    {
+        return -1;
+    }
+
+    if(prevNode == NULL)
+    {
+        newNode->next = curNode;
+        list->head = newNode;
+    }
+    else if (curNode->next == NULL){
+        curNode->next = newNode;
+    }
+    else
+    {
+        prevNode->next = newNode;
+        newNode->next = curNode;
+    }
+
+    return 0;
+
+}
 
 int main()
 {
