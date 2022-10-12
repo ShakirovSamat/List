d9e639c6c3d942917586cdb83124e9bcaef956ca
ShakirovSamat <samatx-r@mail.ru>
2022-10-12 23:06:52 +0300

    add initial file

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <malloc.h>
+#include <stdbool.h>
+
+typedef struct 
+{
+    int value;
+    struct Node *next;
+}Node;
+
+typedef struct 
+{
+    Node *head;
+}List;
+
+int main()
+{
+    return 0;
+}
