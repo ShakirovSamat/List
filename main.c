b3a3374d939216e3de75690ce69a5c3fc9c9cc37
kazbekovruslan <chartusss@yandex.ru>
2022-10-12 23:16:29 +0300

    add len function and test for len function

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stddef.h>
 #include <malloc.h>
+#include <stdbool.h>
 
 
 typedef struct 
@@ -13,6 +14,42 @@ typedef struct
     Node *head;
 }List;
 
+List *create()
+{
+    List *list = calloc(1,sizeof(List));
+    list->head = calloc(1,sizeof(Node));
+    return list;
+}
+
+int len(List *list)
+{
+    if (list == NULL)
+    {
+        return 1;
+    }
+    Node *currentNode = list->head;
+    int length = 0;
+    while (currentNode != NULL)
+    {
+        ++(length);
+        currentNode = currentNode->next;
+    }
+
+    return length;
+}
+
+bool testLenFunction()
+{
+    List *list = create();
+    add(list, 10, 0);
+    add(list, 15, 0);
+    add(list, 30, 0);
+    if (lenList(list) == 3)
+    {
+        return true;
+    }
+    return false;
+}
 
 int main()
 {
