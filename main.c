9cd89f0dc312b3ae006eb527d8496ffd35daed2d
ShakirovSamat <samatx-r@mail.ru>
2022-10-12 23:43:08 +0300

    fixe codeStyle issues, improve test, add print list function, add find function for test

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stddef.h>
 #include <malloc.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 typedef struct 
 {
@@ -22,11 +23,11 @@ List *create()
 
 int add(List *list, int value,int index)
 {
-    if (list == NULL)
+    if (list == NULL || index < 0)
     {
         return -1;
     }
-    if(list->head->value == NULL)
+    if (list->head->value == NULL && index == 0)
     {
         list->head->value = value;
         return 0;
@@ -37,9 +38,9 @@ int add(List *list, int value,int index)
     newNode->value = value;
     
     int i = 0;
-    while(i < index)
+    while (i < index)
     {
-        if(curNode->next == NULL)
+        if (curNode->next == NULL)
         {
             break;
         }
@@ -48,12 +49,12 @@ int add(List *list, int value,int index)
         ++i;
     }
     
-    if (curNode->next == NULL && i < index - 1)
+    if (i < index - 1)
     {
         return -1;
     }
 
-    if(prevNode == NULL)
+    if (prevNode == NULL)
     {
         newNode->next = curNode;
         list->head = newNode;
@@ -68,31 +69,75 @@ int add(List *list, int value,int index)
     }
 
     return 0;
+}
 
+int find(List *list, int index, int *res)
+{
+    if (list == NULL || index < 0)
+    {
+        return -1;
+    }
+
+    Node *curNode = list->head;
+    Node *prevNode = NULL;    
+    int i = 0;
+    while (i < index)
+    {
+        if (curNode->next == NULL)
+        {
+            break;
+        }
+        prevNode = curNode;
+        curNode = curNode->next;
+        ++i;
+    }
+    if (curNode->next == NULL && i < index - 1)
+    {
+        return -1;
+    }
+    *res = curNode->value; 
+}
+
+void printList(List *list)
+{
+    Node *curNode = list->head;
+    printf("List: ");
+    while (curNode != NULL)
+    {
+        printf("%d ", curNode->value);
+        curNode = curNode->next;
+    }
+    printf("\n");
 }
 
 bool testAddFunction()
 {
     List *list = create();
-    if(!add(list, 5, 10)) return false;
-    if(!add(list, 5, 0)) return false;
-    if(!add(list, 10, 0)) return false;
-    if(!add(list, 20, 2)) return false;
-    if(!add(list, 30, -3)) return false;
-    if(!add(list, 50, 3)) return false;
-    if(!add(list, 60, 2)) return false;
-    if(!add(list, 70, 1)) return false;
-    
+    if (!add(list, 5, 10)) return false;
+    if (!add(list, 5, 0)) return false;
+    if (!add(list, 10, 0)) return false;
+    if (!add(list, 20, 2)) return false;
+    if (!add(list, 30, -3)) return false;
+    if (!add(list, 50, 3)) return false;
+    if (!add(list, 60, 2)) return false;
+    if (!add(list, 70, 1)) return false;
+
+    int res = -1000;
+    if (find(list,0, &res) || res != 10) return false;
+    if (find(list,0, &res) || res != 70) return false;
+    if (find(list,0, &res) || res != 5) return false;
+    if (find(list,0, &res) || res != 60) return false;
+    if (find(list,0, &res) || res != 20) return false;
+    if (find(list,0, &res) || res != 50) return false;
     return true;
 }
 int main()
 {
-    if(testAddFunction)
+    if (!testAddFunction)
     {
-        printf("Works");
+        printf("Error in add function");
+        return -1;
     }
-    else{
-        printf("Doesn't work");
-    }
+    
     return 0;
 }
