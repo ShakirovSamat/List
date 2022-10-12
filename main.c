b776c659f2eb8bd6fc0f346156e1ecb8dee9f036
ShakirovSamat <samatx-r@mail.ru>
2022-10-12 23:07:35 +0300

    add find function

--- main.c
+++ main.c
@@ -12,6 +12,33 @@ typedef struct
     Node *head;
 }List;
 
+int find(List *list, int index, int *res)
+{
+    if (list == NULL || index < 0)
+    {
+        return -1;
+    }
+
+    Node *curNode = list->head;
+    Node *prevNode = NULL;    
+    int i = 0;
+    while (i < index)
+    {
+        if (curNode->next == NULL)
+        {
+            break;
+        }
+        prevNode = curNode;
+        curNode = curNode->next;
+        ++i;
+    }
+    if (curNode->next == NULL && i < index - 1)
+    {
+        return -1;
+    }
+    *res = curNode->value; 
+}
+
 int main()
 {
     return 0;
